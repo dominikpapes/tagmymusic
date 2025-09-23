33be6f94a3e5ee00779990493cbe1ab0ef07a327
dominik <notreal@mail.com>
2025-09-23 02:36:01 +0200

    Fix filepaths in bin/lrx.sh

--- bin/lrx.sh
+++ bin/lrx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source 	~/.tagmymusic/.venv/bin/activate
-pip install -r requirements.txt
-python3 ~/.tagmymusic/lrx.py ~/Music/.lyrics
+source 	~/.local/share/lrx/.venv/bin/activate
+python3 ~/.local/share/lrx/lrx.py ~/Music/.lyrics
 deactivate
